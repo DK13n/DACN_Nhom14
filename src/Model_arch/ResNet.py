d6a7f02ef7892e701213e461eb8769d61e915705
DK13n <hoangduykien0312@gmail.com>
2025-09-15 19:57:50 +0700

    feat: Make ResNet branch, output is a feature map needing Embedding to a vector dmodel

---
+++ src/Model_arch/ResNet.py
@@ -0,0 +1,94 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import math
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self,in_ch, block, layers, stop_layer, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_channels = in_ch
+        self.stop_layer = stop_layer
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != out_channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * block.expansion)
+            )
+
+        layers = list([])
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+
+        B, T, C, H, W = x.shape
+        x = x.view(B*T,C,H,W)
+
+
+        x = self.layer1(x)
+
+        if self.stop_layer == 1:
+            return x.view(B,T,x.shape(1),x.shape(2),x.shape(3))
+
+        x = self.layer2(x)
+
+        if self.stop_layer == 2:
+            return x.view(B, T, x.shape(1), x.shape(2), x.shape(3))
+
+        x = self.layer3(x)
+
+        if self.stop_layer == 3:
+            return x.view(B, T, x.shape(1), x.shape(2), x.shape(3))
+
+        x = self.layer4(x)
+
+        return x.view(B, T, x.shape(1), x.shape(2), x.shape(3))
