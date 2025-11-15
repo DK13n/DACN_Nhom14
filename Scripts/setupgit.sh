05d3a9710060faac5f546aa77c9070e43789b150
ThanhVV <Thanh2005.hs@gmail.com>
2025-11-15 17:09:31 +0000

    chore

--- Scripts/setupgit.sh
+++ Scripts/setupgit.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+git config user.name "ThanhVV"
+git config user.email "Thanh2005.hs@gmail.com"
+
 ssh-keygen -t ed25519 -C "Thanh2005.hs@gmail.com"
 cat ~/.ssh/id_ed25519.pub
 
+git remote -v
 
-git remote add origin git@github.com:DK13n/DACN_Nhom14.git
+ssh -T git@github.com
 
-git clone -b ThanhVV --single-branch https://github.com/DK13n/DACN_Nhom14.git
+#git remote add origin git@github.com:DK13n/DACN_Nhom14.git
+
+#git clone -b ThanhVV --single-branch https://github.com/DK13n/DACN_Nhom14.git
